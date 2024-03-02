50b6cacd74e4649cb328a82e79aac0244699d7d6
HyeonseoLeeB <hyeonseo_lee@brown.edu>
2024-03-02 16:10:28 -0500

    test

--- R/estimate_beta.r
+++ R/estimate_beta.r
@@ -10,16 +10,7 @@
 #' estimate_beta(3, 5)
 #' 
 estimate_beta <- function(y, X) {
-    if (length(y) != nrow(X)) {
-    stop("Dimensions of 'y' and 'X' are not compatible.")
-  }
   beta_hat <- solve(t(X) %*% X) %*% t(X) %*% y
   return(beta_hat)
 }
 
-
-#test
-#y <- c(2, 4)
-#X <- c(2, 4)
-#beta_hat <- estimate_beta(y, X)
-#print(beta_hat)
