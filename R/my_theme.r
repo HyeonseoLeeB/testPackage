a0867798af8eb0c00a5b16ff8af12a71707fed54
HyeonseoLeeB <hyeonseo_lee@brown.edu>
2024-03-02 14:58:27 -0500

    Wrote two new functions

---
+++ R/my_theme.r
@@ -0,0 +1,17 @@
+my_theme <- function() {
+  # Adjustments to the default theme
+  custom_theme <- theme_dark() +
+    theme(
+      text = element_text(size = 20, color = "blue"),
+      panel.background = element_rect(fill = "#d8f0f5"),
+      axis.text = element_text(color = "#6a3302"), 
+    )
+  
+  return(custom_theme)
+}
+
+# Example plot
+#lot_data <- data.frame(x = 1:5, y = c(2, 4, 5, 8, 10))
+#ggplot(plot_data, aes(x, y)) +
+#  geom_point() +
+#  my_theme()  # Apply the custom theme
--- codes.r
+++ codes.r
@@ -3,3 +3,12 @@ usethis::create_package(".")
 2
 devtools::document()
 usethis::use_mit_license()
+
+install.packages("tidyverse")
+install.packages("ggplot2")
+library(ggplot2)
+head(mtcars)
+
+usethis::use_package("ggplot2")
+
+devtools::check()
